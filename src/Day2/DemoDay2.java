c4ca4025190bc302ec2c6510b2bfe835fc4487c3
chuchu <chuchu@rands-air.lan>
2025-01-31 18:07:40 -0500

    Day2 Practice

---
+++ src/Day2/DemoDay2.java
@@ -0,0 +1,11 @@
+package Day2;
+
+public class DemoDay2 {
+	public static void main (String args []) {
+		System.out.println("JAVA");
+					System.out.println("Shahed");
+		
+								System.out.println("Chocolate");
+
+	}
+}
