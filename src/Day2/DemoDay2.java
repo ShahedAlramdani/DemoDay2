7861d24d4161be52fedf04e5a23254c16b682216
chuchu <chuchu@rands-air.lan>
2025-01-31 18:19:02 -0500

    new code added

--- src/Day2/DemoDay2.java
+++ src/Day2/DemoDay2.java
@@ -6,6 +6,10 @@ public class DemoDay2 {
 					System.out.println("Shahed");
 		
 								System.out.println("Chocolate");
+								int a = 10;
+								int b = 20;
+								System.out.println(a+b);
+								
 
 	}
 }
